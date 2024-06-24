11290e898acf3b40409fc3a026c96253014fd3bc
Maryna Binko <benyhorses@hotmail.com>
2024-06-24 12:35:13 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import { useDispatch } from 'react-redux'
 import './App.css'
+import { useEffect } from 'react';
+import { Switch } from '@mui/material';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { refreshUser } from "./redux/auth/authOps";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
+import HomePage from "./pages/HomePage/HomePage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import Layout from "./components/Layout/Layout";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(refreshUser());
+  },[dispatch]);
  
 
   return (
-    <>
-  
-    </>
-  )
-}
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <RestrictedRoute exact path="/register" component={RegistrationPage}/>
+          <RestrictedRoute exact path="/login" component={LoginPage}/>
+          <PrivateRoute exact path="/contacts" component={ContactsPage}/>
+        </Switch>
+      </Layout>  
+    </Router>
+  );
+};
 
 export default App
