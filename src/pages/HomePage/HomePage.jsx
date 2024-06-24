65bf2c5bfadc0626df8e9e9ca3429f4c97495e4c
Maryna Binko <benyhorses@hotmail.com>
2024-06-24 13:17:08 +0100

    login form

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>Welcome to Phonebook App</h1>      
+      
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,11 @@
+import RegistrationForm from "../../components/ContctForm/ContctForm"
+
+const RegistrationPage = () => {
+  return (
+    <div>
+      <RegistrationForm/>
+    </div>
+  )
+}
+
+export default RegistrationPage
