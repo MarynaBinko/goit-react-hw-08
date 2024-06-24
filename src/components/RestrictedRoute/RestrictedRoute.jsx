19677a8eec73967318edd2a7180011d60b6a68e0
Maryna Binko <benyhorses@hotmail.com>
2024-06-24 12:54:46 +0100

    restricted route

---
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux"
+import { Route } from "react-router-dom"
+
+
+const RestrictedRoute = ({component: Component, isLoggedIn, ...rest}) => {
+    const isAuthenticated = useSelector((state)=> state.auth.isLoggedIn)
+  return (
+   <Route
+   {...rest}
+   render={(props)=>
+    isAuthenticated ? (
+        <Redirect to="/contacts"/>
+    ) : (
+        <Component {...props}/>
+    )
+   }
+   />
+  );
+};
+
+export default RestrictedRoute
