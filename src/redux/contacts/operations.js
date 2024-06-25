d432ba056711e179fe01be6dcc89dfbb4d238210
Maryna Binko <benyhorses@hotmail.com>
2024-06-25 14:22:22 +0100

    current user

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -2,14 +2,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 
-// const BASE_URL = 'https://connections-api.goit.global/contacts';
+const BASE_URL = 'https://connections-api.goit.global/contacts';
 
 // GET @ /contacts
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const res = await axios.get('/contacts');
+      const res = await axios.get(BASE_URL);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -22,7 +22,7 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
     try {
-      const response = await axios.post('/contacts', { contact });
+      const response = await axios.post(BASE_URL, contact);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -35,7 +35,7 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, thunkAPI) => {
     try {
-      await axios.delete(`/contacts/${contactId}`);
+      await axios.delete(`${BASE_URL}/${contactId}`);
       return contactId;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
